7ba9c8c5c67e90dd87bc0ac90f9663bbd2555ee1
Phil Morgan <phil@psmorgan.co.uk>
2025-05-02 09:52:49 +0100

    BAU: Copy over rest of risk assessment logic

--- src/Processor/Models/ImportNotification/ImportNotification.cs
+++ src/Processor/Models/ImportNotification/ImportNotification.cs
@@ -101,9 +101,104 @@ public class ImportNotification
     [JsonPropertyName("isGMRMatched")]
     public bool? IsGMRMatched { get; init; }
 
+    private static string FromSnakeCase(string input)
+    {
+        if (input == "netweight")
+        {
+            return "netWeight";
+        }
+
+        var pascal = input
+            .Split(["_"], StringSplitOptions.RemoveEmptyEntries)
+            .Select(s => char.ToUpperInvariant(s[0]) + s.Substring(1, s.Length - 1))
+            .Aggregate(string.Empty, (s1, s2) => s1 + s2);
+        return char.ToLower(pascal[0]) + pascal[1..];
+    }
+
+    private static IDictionary<string, object> FromSnakeCase(IDictionary<string, object>? input)
+    {
+        return input == null
+            ? new Dictionary<string, object>()
+            : input.ToDictionary(mc => FromSnakeCase(mc.Key), mc => mc.Value);
+    }
+
     public static explicit operator DataApiIpaffs.ImportPreNotification(ImportNotification importNotification)
     {
         var commodities = importNotification.PartOne?.Commodities;
+
+        if (commodities?.CommodityComplements?.Length == 1)
+        {
+            commodities.CommodityComplements[0].AdditionalData = FromSnakeCase(
+                commodities.ComplementParameterSets![0].KeyDataPairs!
+            );
+            if (importNotification.RiskAssessment != null)
+            {
+                commodities.CommodityComplements[0].RiskAssesment = importNotification.RiskAssessment.CommodityResults![
+                    0
+                ];
+            }
+        }
+        else
+        {
+            var complementParameters = new Dictionary<int, ComplementParameterSet>();
+            var complementRiskAssessments = new Dictionary<string, CommodityRiskResult>();
+            var commodityChecks = new Dictionary<string, InspectionCheck[]>();
+
+            if (commodities?.ComplementParameterSets != null)
+            {
+                foreach (var commoditiesCommodityComplement in commodities.ComplementParameterSets)
+                {
+                    complementParameters[commoditiesCommodityComplement.ComplementId!.Value] =
+                        commoditiesCommodityComplement;
+                }
+            }
+
+            if (importNotification.RiskAssessment?.CommodityResults != null)
+            {
+                foreach (var commoditiesRa in importNotification.RiskAssessment.CommodityResults)
+                {
+                    complementRiskAssessments[commoditiesRa.UniqueId!] = commoditiesRa;
+                }
+            }
+
+            if (importNotification.PartTwo?.CommodityChecks != null)
+            {
+                foreach (var commodityCheck in importNotification.PartTwo.CommodityChecks!)
+                {
+                    commodityChecks[commodityCheck.UniqueComplementId!] = commodityCheck.Checks!;
+                }
+            }
+
+            if (commodities?.CommodityComplements is not null)
+            {
+                foreach (var commodity in commodities.CommodityComplements)
+                {
+                    var parameters = complementParameters[commodity.ComplementId!.Value];
+                    commodity.AdditionalData = FromSnakeCase(parameters.KeyDataPairs!);
+
+                    if (
+                        complementRiskAssessments.Count != 0
+                        && parameters.UniqueComplementId is not null
+                        && complementRiskAssessments.TryGetValue(
+                            parameters.UniqueComplementId,
+                            out var riskAssessmentValue
+                        )
+                    )
+                    {
+                        commodity.RiskAssesment = riskAssessmentValue;
+                    }
+
+                    if (
+                        commodityChecks.Count != 0
+                        && parameters.UniqueComplementId is not null
+                        && commodityChecks.TryGetValue(parameters.UniqueComplementId, out var checksValue)
+                    )
+                    {
+                        commodity.Checks = checksValue;
+                    }
+                }
+            }
+        }
 
         return new DataApiIpaffs.ImportPreNotification
         {
