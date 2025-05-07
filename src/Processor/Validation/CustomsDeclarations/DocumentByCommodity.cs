4677a0c7e7f5b8278fbc063ce4ca8d81a22dcbce
Phil Morgan <phil@psmorgan.co.uk>
2025-05-07 14:08:02 +0100

    CDMS-462: Add the 9HCG code
    This change is unrelated to CDMS-462 but it was identified during review of the ACs that 9HCG is not present in the doc to check code map.
    
    9HCG is a legacy code that is unlikely to ever appear in the system, but it has been added just in case.
    
    DocumentByCommodity was unused.

--- src/Processor/Validation/CustomsDeclarations/DocumentByCommodity.cs
+++
@@ -1,9 +0,0 @@
-using Defra.TradeImportsDataApi.Domain.CustomsDeclaration;
-
-namespace Defra.TradeImportsProcessor.Processor.Validation.CustomsDeclarations;
-
-public record DocumentByCommodity
-{
-    public required Commodity Commodity { get; init; }
-    public required ImportDocument Document { get; init; }
-}
