003a7832284bccd6fbc9ea113685b17ee1b11012
Tim Peel <tim.peel@equalexperts.com>
2025-05-08 12:53:30 +0100

    Add extended health checks so we can see AWS and ASB configuration

---
+++ src/Processor/Health/AsbHealthCheckBuilderExtensions.cs
@@ -0,0 +1,68 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Net;
+using Azure.Messaging.ServiceBus;
+using Defra.TradeImportsProcessor.Processor.Configuration;
+using HealthChecks.AzureServiceBus;
+using HealthChecks.AzureServiceBus.Configuration;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace Defra.TradeImportsProcessor.Processor.Health;
+
+[ExcludeFromCodeCoverage]
+public static class AsbHealthCheckBuilderExtensions
+{
+    public static IHealthChecksBuilder AddAsbTopic(
+        this IHealthChecksBuilder builder,
+        string name,
+        Func<IServiceProvider, ServiceBusSubscriptionOptions> subscriptionFunc,
+        IEnumerable<string>? tags = null,
+        TimeSpan? timeout = null
+    )
+    {
+        builder.Add(
+            new HealthCheckRegistration(
+                name,
+                sp => CreateHealthCheck(sp, subscriptionFunc(sp)),
+                HealthStatus.Unhealthy,
+                tags,
+                timeout
+            )
+        );
+
+        return builder;
+    }
+
+    private static AzureServiceBusSubscriptionHealthCheck CreateHealthCheck(
+        IServiceProvider serviceProvider,
+        ServiceBusSubscriptionOptions subscription
+    )
+    {
+        var options = new AzureServiceBusSubscriptionHealthCheckHealthCheckOptions(
+            subscription.Topic,
+            subscription.Subscription
+        )
+        {
+            ConnectionString = subscription.ConnectionString,
+            UsePeekMode = true,
+        };
+
+        return new AzureServiceBusSubscriptionHealthCheck(options, new ServiceBusClientProvider(serviceProvider));
+    }
+
+    private sealed class ServiceBusClientProvider(IServiceProvider serviceProvider)
+        : HealthChecks.AzureServiceBus.ServiceBusClientProvider
+    {
+        public override ServiceBusClient CreateClient(string? connectionString)
+        {
+            var clientOptions = serviceProvider.GetRequiredService<IHostEnvironment>().IsDevelopment()
+                ? new ServiceBusClientOptions()
+                : new ServiceBusClientOptions
+                {
+                    WebProxy = serviceProvider.GetRequiredService<IWebProxy>(),
+                    TransportType = ServiceBusTransportType.AmqpWebSockets,
+                };
+
+            return new ServiceBusClient(connectionString, clientOptions);
+        }
+    }
+}
---
+++ src/Processor/Health/ServiceCollectionExtensions.cs
@@ -0,0 +1,36 @@
+using System.Diagnostics.CodeAnalysis;
+using Defra.TradeImportsProcessor.Processor.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace Defra.TradeImportsProcessor.Processor.Health;
+
+[ExcludeFromCodeCoverage]
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddHealth(this IServiceCollection services, IConfiguration configuration)
+    {
+        services
+            .AddHealthChecks()
+            .AddAsbTopic(
+                "IPAFFS notifications",
+                sp => sp.GetRequiredService<IOptions<ServiceBusOptions>>().Value.Notifications,
+                tags: [WebApplicationExtensions.Extended],
+                timeout: TimeSpan.FromSeconds(10)
+            )
+            .AddAsbTopic(
+                "DMP GMRs",
+                sp => sp.GetRequiredService<IOptions<ServiceBusOptions>>().Value.Gmrs,
+                tags: [WebApplicationExtensions.Extended],
+                timeout: TimeSpan.FromSeconds(10)
+            )
+            .AddSqs(
+                configuration,
+                "Gateway customs declarations",
+                sp => sp.GetRequiredService<IOptions<CustomsDeclarationsConsumerOptions>>().Value.QueueName,
+                tags: [WebApplicationExtensions.Extended],
+                timeout: TimeSpan.FromSeconds(10)
+            );
+
+        return services;
+    }
+}
