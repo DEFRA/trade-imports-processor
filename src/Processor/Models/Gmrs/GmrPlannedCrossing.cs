af3ef63c4dcde18901e8e4c79f513dddf99516ea
Tim Peel <tim.peel@equalexperts.com>
2025-06-24 08:45:04 +0100

    Planned crossing - local date time of departure appears to be nullable based on production observations (#163)

--- src/Processor/Models/Gmrs/GmrPlannedCrossing.cs
+++ src/Processor/Models/Gmrs/GmrPlannedCrossing.cs
@@ -2,6 +2,6 @@ namespace Defra.TradeImportsProcessor.Processor.Models.Gmrs;
 
 public class GmrPlannedCrossing
 {
-    public DateTime LocalDateTimeOfDeparture { get; init; }
+    public DateTime? LocalDateTimeOfDeparture { get; init; }
     public string? RouteId { get; init; }
 }
