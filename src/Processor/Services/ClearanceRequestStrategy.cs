5e32f1e585260056d16fd35bb9287c655f213c72
Tim Peel <tim.peel@equalexperts.com>
2025-08-27 08:50:22 +0100

    Add topic health check for alvs_topic (#229)
    * Add topic health check for alvs_topic
    
    * Update logging to include message type

--- src/Processor/Services/ClearanceRequestStrategy.cs
+++ src/Processor/Services/ClearanceRequestStrategy.cs
@@ -39,6 +39,12 @@ public class ClearanceRequestStrategy(IMessageBus azureServiceBus, ILogger<Clear
             },
             cancellationToken: cancellationToken
         );
-        logger.LogInformation("{MRN} Message successfully published to IPAFFS for {MessageId}", resourceId, messageId);
+
+        logger.LogInformation(
+            "{MRN} {MessageType} Message successfully published to IPAFFS for {MessageId}",
+            resourceId,
+            ClearanceRequestMessageType,
+            messageId
+        );
     }
 }
