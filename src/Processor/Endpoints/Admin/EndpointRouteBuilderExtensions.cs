270141028fd459c4c64d2e4368cab203c07aa0ad
Thomas Andoerson <t11omas1983@live.co.uk>
2025-11-19 15:21:41 +0000

    PR feedback

--- src/Processor/Endpoints/Admin/EndpointRouteBuilderExtensions.cs
+++ src/Processor/Endpoints/Admin/EndpointRouteBuilderExtensions.cs
@@ -12,8 +12,8 @@ public static class EndpointRouteBuilderExtensions
 {
     public static void MapAdminEndpoints(this IEndpointRouteBuilder app)
     {
-        app.MapPost("admin/resource-events/dlq/redrive", Redrive)
-            .WithName(nameof(Redrive))
+        app.MapPost("admin/resource-events/dlq/redrive", RedriveResourceEvents)
+            .WithName(nameof(RedriveResourceEvents))
             .WithTags("Admin")
             .WithSummary("Initiates redrive of messages from the dead letter queue")
             .WithDescription("Redrives all messages on the resource events dead letter queue")
@@ -24,8 +24,8 @@ public static class EndpointRouteBuilderExtensions
             .ProducesProblem(StatusCodes.Status500InternalServerError)
             .RequireAuthorization(PolicyNames.Execute);
 
-        app.MapPost("admin/resource-events/dlq/remove-message", RemoveMessage)
-            .WithName(nameof(RemoveMessage))
+        app.MapPost("admin/resource-events/dlq/remove-message", RemoveResourceEventsMessage)
+            .WithName(nameof(RemoveResourceEventsMessage))
             .WithTags("Admin")
             .WithSummary("Initiates removal of message from the dead letter queue")
             .WithDescription(
@@ -38,8 +38,8 @@ public static class EndpointRouteBuilderExtensions
             .ProducesProblem(StatusCodes.Status500InternalServerError)
             .RequireAuthorization(PolicyNames.Execute);
 
-        app.MapPost("admin/resource-events/dlq/drain", Drain)
-            .WithName(nameof(Drain))
+        app.MapPost("admin/resource-events/dlq/drain", DrainResourceEvents)
+            .WithName(nameof(DrainResourceEvents))
             .WithTags("Admin")
             .WithSummary("Initiates drain of all messages from the dead letter queue")
             .WithDescription("Drains all messages on the resource events dead letter queue")
@@ -50,11 +50,11 @@ public static class EndpointRouteBuilderExtensions
             .ProducesProblem(StatusCodes.Status500InternalServerError)
             .RequireAuthorization(PolicyNames.Execute);
 
-        app.MapPost("admin/customs-declarations/dlq/redrive", Redrive)
+        app.MapPost("admin/customs-declarations/dlq/redrive", RedriveCustomsDeclarations)
             .WithName(nameof(RedriveCustomsDeclarations))
             .WithTags("Admin")
-            .WithSummary("Initiates redrive of messages from the dead letter queue")
-            .WithDescription("Redrives all messages on the resource events dead letter queue")
+            .WithSummary("Initiates redrive of messages from the Customs Declarations dead letter queue")
+            .WithDescription("Redrives all messages on the Customs Declarations dead letter queue")
             .Produces(StatusCodes.Status202Accepted)
             .ProducesProblem(StatusCodes.Status401Unauthorized)
             .ProducesProblem(StatusCodes.Status403Forbidden)
@@ -62,12 +62,12 @@ public static class EndpointRouteBuilderExtensions
             .ProducesProblem(StatusCodes.Status500InternalServerError)
             .RequireAuthorization(PolicyNames.Execute);
 
-        app.MapPost("admin/customs-declarations/dlq/remove-message", RemoveMessage)
+        app.MapPost("admin/customs-declarations/dlq/remove-message", RemoveCustomsDeclarationsMessage)
             .WithName(nameof(RemoveCustomsDeclarationsMessage))
             .WithTags("Admin")
-            .WithSummary("Initiates removal of message from the dead letter queue")
+            .WithSummary("Initiates removal of message from the Customs Declarations dead letter queue")
             .WithDescription(
-                "Attempts to find and remove a message on the resource events dead letter queue by message ID"
+                "Attempts to find and remove a message on the Customs Declarations dead letter queue by message ID"
             )
             .Produces(StatusCodes.Status200OK)
             .ProducesProblem(StatusCodes.Status401Unauthorized)
@@ -76,11 +76,11 @@ public static class EndpointRouteBuilderExtensions
             .ProducesProblem(StatusCodes.Status500InternalServerError)
             .RequireAuthorization(PolicyNames.Execute);
 
-        app.MapPost("admin/customs-declarations/dlq/drain", Drain)
+        app.MapPost("admin/customs-declarations/dlq/drain", DrainCustomsDeclarations)
             .WithName(nameof(DrainCustomsDeclarations))
             .WithTags("Admin")
-            .WithSummary("Initiates drain of all messages from the dead letter queue")
-            .WithDescription("Drains all messages on the resource events dead letter queue")
+            .WithSummary("Initiates drain of all messages from the Customs Declarations dead letter queue")
+            .WithDescription("Drains all messages on the Customs Declarations dead letter queue")
             .Produces(StatusCodes.Status200OK)
             .ProducesProblem(StatusCodes.Status401Unauthorized)
             .ProducesProblem(StatusCodes.Status403Forbidden)
@@ -90,21 +90,88 @@ public static class EndpointRouteBuilderExtensions
     }
 
     [HttpPost]
-    private static async Task<IResult> Redrive(
+    private static Task<IResult> RedriveResourceEvents(
         [FromServices] ISqsDeadLetterService sqsDeadLetterService,
         [FromServices] IOptions<ResourceEventsConsumerOptions> options,
         CancellationToken cancellationToken
     )
     {
+        return Redrive(
+            sqsDeadLetterService,
+            options.Value.DeadLetterQueueName,
+            options.Value.QueueName,
+            cancellationToken
+        );
+    }
+
+    [HttpPost]
+    private static Task<IResult> RemoveResourceEventsMessage(
+        string messageId,
+        [FromServices] ISqsDeadLetterService sqsDeadLetterService,
+        [FromServices] IOptions<ResourceEventsConsumerOptions> options,
+        CancellationToken cancellationToken
+    )
+    {
+        return RemoveMessage(messageId, sqsDeadLetterService, options.Value.DeadLetterQueueName, cancellationToken);
+    }
+
+    [HttpPost]
+    private static Task<IResult> DrainResourceEvents(
+        [FromServices] ISqsDeadLetterService sqsDeadLetterService,
+        [FromServices] IOptions<ResourceEventsConsumerOptions> options,
+        CancellationToken cancellationToken
+    )
+    {
+        return Drain(sqsDeadLetterService, options.Value.DeadLetterQueueName, cancellationToken);
+    }
+
+    [HttpPost]
+    private static Task<IResult> RedriveCustomsDeclarations(
+        [FromServices] ISqsDeadLetterService sqsDeadLetterService,
+        [FromServices] IOptions<CustomsDeclarationsConsumerOptions> options,
+        CancellationToken cancellationToken
+    )
+    {
+        return Redrive(
+            sqsDeadLetterService,
+            options.Value.DeadLetterQueueName,
+            options.Value.QueueName,
+            cancellationToken
+        );
+    }
+
+    [HttpPost]
+    private static Task<IResult> RemoveCustomsDeclarationsMessage(
+        string messageId,
+        [FromServices] ISqsDeadLetterService sqsDeadLetterService,
+        [FromServices] IOptions<CustomsDeclarationsConsumerOptions> options,
+        CancellationToken cancellationToken
+    )
+    {
+        return RemoveMessage(messageId, sqsDeadLetterService, options.Value.DeadLetterQueueName, cancellationToken);
+    }
+
+    [HttpPost]
+    private static Task<IResult> DrainCustomsDeclarations(
+        [FromServices] ISqsDeadLetterService sqsDeadLetterService,
+        [FromServices] IOptions<CustomsDeclarationsConsumerOptions> options,
+        CancellationToken cancellationToken
+    )
+    {
+        return Drain(sqsDeadLetterService, options.Value.DeadLetterQueueName, cancellationToken);
+    }
+
+    [HttpPost]
+    private static async Task<IResult> Redrive(
+        ISqsDeadLetterService sqsDeadLetterService,
+        string sourceQueue,
+        string destinationQueue,
+        CancellationToken cancellationToken
+    )
+    {
         try
         {
-            if (
-                !await sqsDeadLetterService.Redrive(
-                    options.Value.DeadLetterQueueName,
-                    options.Value.QueueName,
-                    cancellationToken
-                )
-            )
+            if (!await sqsDeadLetterService.Redrive(sourceQueue, destinationQueue, cancellationToken))
             {
                 return Results.InternalServerError();
             }
@@ -120,18 +187,14 @@ public static class EndpointRouteBuilderExtensions
     [HttpPost]
     private static async Task<IResult> RemoveMessage(
         string messageId,
-        [FromServices] ISqsDeadLetterService sqsDeadLetterService,
-        [FromServices] IOptions<ResourceEventsConsumerOptions> options,
+        ISqsDeadLetterService sqsDeadLetterService,
+        string queue,
         CancellationToken cancellationToken
     )
     {
         try
         {
-            var result = await sqsDeadLetterService.Remove(
-                messageId,
-                options.Value.DeadLetterQueueName,
-                cancellationToken
-            );
+            var result = await sqsDeadLetterService.Remove(messageId, queue, cancellationToken);
 
             return Results.Content(result, "text/plain; charset=utf-8");
         }
@@ -143,88 +206,14 @@ public static class EndpointRouteBuilderExtensions
 
     [HttpPost]
     private static async Task<IResult> Drain(
-        [FromServices] ISqsDeadLetterService sqsDeadLetterService,
-        [FromServices] IOptions<ResourceEventsConsumerOptions> options,
+        ISqsDeadLetterService sqsDeadLetterService,
+        string queue,
         CancellationToken cancellationToken
     )
     {
         try
         {
-            if (!await sqsDeadLetterService.Drain(options.Value.DeadLetterQueueName, cancellationToken))
-            {
-                return Results.InternalServerError();
-            }
-        }
-        catch (Exception)
-        {
-            return Results.InternalServerError();
-        }
-
-        return Results.Ok();
-    }
-
-    [HttpPost]
-    private static async Task<IResult> RedriveCustomsDeclarations(
-        [FromServices] ISqsDeadLetterService sqsDeadLetterService,
-        [FromServices] IOptions<CustomsDeclarationsConsumerOptions> options,
-        CancellationToken cancellationToken
-    )
-    {
-        try
-        {
-            if (
-                !await sqsDeadLetterService.Redrive(
-                    options.Value.DeadLetterQueueName,
-                    options.Value.QueueName,
-                    cancellationToken
-                )
-            )
-            {
-                return Results.InternalServerError();
-            }
-        }
-        catch (Exception)
-        {
-            return Results.InternalServerError();
-        }
-
-        return Results.Accepted();
-    }
-
-    [HttpPost]
-    private static async Task<IResult> RemoveCustomsDeclarationsMessage(
-        string messageId,
-        [FromServices] ISqsDeadLetterService sqsDeadLetterService,
-        [FromServices] IOptions<CustomsDeclarationsConsumerOptions> options,
-        CancellationToken cancellationToken
-    )
-    {
-        try
-        {
-            var result = await sqsDeadLetterService.Remove(
-                messageId,
-                options.Value.DeadLetterQueueName,
-                cancellationToken
-            );
-
-            return Results.Content(result, "text/plain; charset=utf-8");
-        }
-        catch (Exception)
-        {
-            return Results.InternalServerError();
-        }
-    }
-
-    [HttpPost]
-    private static async Task<IResult> DrainCustomsDeclarations(
-        [FromServices] ISqsDeadLetterService sqsDeadLetterService,
-        [FromServices] IOptions<CustomsDeclarationsConsumerOptions> options,
-        CancellationToken cancellationToken
-    )
-    {
-        try
-        {
-            if (!await sqsDeadLetterService.Drain(options.Value.DeadLetterQueueName, cancellationToken))
+            if (!await sqsDeadLetterService.Drain(queue, cancellationToken))
             {
                 return Results.InternalServerError();
             }
